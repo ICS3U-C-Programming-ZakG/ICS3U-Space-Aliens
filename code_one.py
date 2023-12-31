9057193390278ae28957b20ec3a0e9c874966587
Zak Goneau <zak.goneau@stu.ocsb.ca>
2023-12-31 18:06:34 +0000

    This program is the Space Aliens CPT on the PyBadge, first day, and added a Hello, Zak message and created the main game game scene.

---
+++ code_one.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Created by: Zak Goneau
+# Created on: Dec. 31, 2023
+# This program is the Space Aliens program on the PyBadge.
+
+import ugame
+import stage
+
+def game_scene():
+
+    # this is main game game_scene
+    print("\n\n\n")
+    print("Hello, Zak")
+
+    # game loop
+    while True:
+
+        # placeholder
+        pass
+
+if __name__ == "__main__":
+    game_scene()
