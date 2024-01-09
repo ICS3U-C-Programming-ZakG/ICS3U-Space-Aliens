e2bbdf5cb8a44722b3266412ecbba15f62356722
Zak Goneau <zak.goneau@stu.ocsb.ca>
2024-01-09 17:44:19 +0000

    Added a ship sprite in the middle of the screen.

--- code.py
+++ code.py
@@ -13,14 +13,20 @@ def game_scene():
     # import background and assign to a variable
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
 
+    # import image for sprite and assign
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
+
     # grid for image background
     background = stage.Grid(image_bank_background, 10, 8)
+
+    # create a single sprite,  get fifth image, and place in middle fo the screen
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # display images and set refresh rate to 60 hertz
     game = stage.Stage(ugame.display, 60)
 
-    # put image of background into a list assigned to game
-    game.layers = [background]
+    # put image of background and ship into a list assigned to game
+    game.layers = [ship] + [background]
 
     # display background
     game.render_block()
@@ -28,8 +34,14 @@ def game_scene():
     # game loop
     while True:
 
-        # placeholder
-        pass
+        # get user input
+
+
+        # update game logic
+
+        # redraw sprites
+        game.render_sprites([ship])
+        game.tick()
 
 if __name__ == "__main__":
     game_scene()
