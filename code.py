022f1ad83facf361d58f937b3e27a9082c85c0f7
Zak Goneau <zak.goneau@stu.ocsb.ca>
2024-01-10 18:09:58 +0000

    Added boundaries on all sides of the screen, ship can still freely move around, created a constants file and used constants rather than strict numbers.

--- code.py
+++ code.py
@@ -6,6 +6,8 @@
 import stage
 import ugame
 
+import constants
+
 def game_scene():
 
     # this is the main game game_scene
@@ -17,13 +19,13 @@ def game_scene():
     image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
 
     # grid for image background
-    background = stage.Grid(image_bank_background, 10, 8)
+    background = stage.Grid(image_bank_background, constants.SCREEN_GRID_X, constants.SCREEN_GRID_Y)
 
-    # create a single sprite,  get fifth image, and place in middle fo the screen
-    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
+    # create a single sprite,  get fifth image, and place in middle of the screen
+    ship = stage.Sprite(image_bank_sprites, 5, 75, constants.SCREEN_Y - constants.SPRITE_SIZE)
 
     # display images and set refresh rate to 60 hertz
-    game = stage.Stage(ugame.display, 60)
+    game = stage.Stage(ugame.display, constants.FPS)
 
     # put image of background and ship into a list assigned to game
     game.layers = [ship] + [background]
@@ -34,28 +36,70 @@ def game_scene():
     # game loop
     while True:
 
-        # get user input
+        # get user button input
         keys = ugame.buttons.get_pressed()
 
         # check what button is being pressed then do action corresponding to that input
         if keys & ugame.K_X:
-            print("A")
+            pass
         if keys & ugame.K_O:
-            print("B")
+            pass
         if keys & ugame.K_START:
-            print("Start")
+            pass
         if keys & ugame.K_SELECT:
-            print("Select")
+            pass
         
-        # check for movement if so move one pixel to corresponding button direction
+        # check if right button is pressed
         if keys & ugame.K_RIGHT:
-            ship.move(ship.x + 1, ship.y)
+
+            # check if ship is within boundaries
+            if ship.x <= constants.SCREEN_X - constants.SPRITE_SIZE:
+
+                # move ship 1 pixel right
+                ship.move(ship.x + 1, ship.y)
+            
+            else:
+                # prevent ship from passing boundary, the screen
+                ship.move(constants.SCREEN_X - constants.SPRITE_SIZE, ship.y)
+
+        # check if left button is pressed
         if keys & ugame.K_LEFT:
-            ship.move(ship.x - 1, ship.y)
+
+            # check if ship is within boundaries
+            if ship.x >= 0:
+
+                # move ship 1 pixel left
+                ship.move(ship.x - 1, ship.y)
+            
+            # else stay at x = 0 to not pass boundary
+            else:
+                ship.move(0, ship.y)
+
+        # check if the up button is being pressed
         if keys & ugame.K_UP:
-            ship.move(ship.x, ship.y - 1)
+
+            # check if ship is in boundaries
+            if ship.y >= 0:
+
+                # move ship up 1 pixel
+                ship.move(ship.x, ship.y - 1)
+            
+            # prevent ship from getting out of the boundaries
+            else:
+                ship.move(ship.x, 0)
+        
+        # check if down button is being pressed
         if keys & ugame.K_DOWN:
-            ship.move(ship.x, ship.y + 1)
+
+            # check if ship is in boundaries
+            if ship.y <= constants.SCREEN_Y - constants.SPRITE_SIZE:
+
+                # move ship 1 pixel down
+                ship.move(ship.x, ship.y + 1)
+            
+            # prevent ship from getting out of bounds
+            else:
+                ship.move(ship.x, constants.SCREEN_Y - constants.SPRITE_SIZE)
 
         # update game logic
 
---
+++ constants.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created by: Zak Goneau
+# Created on: Jan. 10, 2024
+# This program contains constants for the Space Aliens game.
+
+# PyBadge screen size is 160x128
+SCREEN_X = 160
+SCREEN_Y = 128
+
+# grid of the screen, how much of the 16x16 images it can fit
+SCREEN_GRID_X = 10
+SCREEN_GRID_Y = 8
+
+# size of sprite
+SPRITE_SIZE = 16
+
+# 60 FPS
+FPS = 60
+
+# Movement speed/how much it moves
+SPRITE_MOVEMENT_SPEED = 1
